fd4b6c140ad068766ea992d9d9b26fe940bba4f6
Crowdbotics <engineering@crowdbotics.com>
2023-03-06 06:01:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/calm_wave_dev_68026/MainActivity.java
@@ -0,0 +1,15 @@
+package com.calm_wave_dev_68026;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "calm_wave_dev_68026";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-wave-dev-68026",
+  url: "https://calm-wave-dev-68026.botics.co",
+  api: "https://calm-wave-dev-68026.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
